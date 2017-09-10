3dc40e7fbee2d00d40d251cea1a957da5ba7cddd
ErickPeiker <erick.peiker@hotmail.com>
2017-09-10 20:27:29 -0300

    IMplementação da autenticação por TOKEN

---
+++ src/main/java/com/udemy/api/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.udemy.api.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.udemy.api.entities.Funcionario;
+import com.udemy.api.security.JwtUserFactory;
+import com.udemy.api.services.FuncionarioService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
